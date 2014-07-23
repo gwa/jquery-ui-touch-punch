2a3ca2463eb204dec2eaf5758cd8c7554da5f265
Timothy Groves <groves@greatwhiteark.com>
2014-07-23 18:42:16 +0200

    Updated grunt to 0.4, added dependencies
    Use `grunt-contrib-jshint` and `grunt-contrib-uglify`, added to node
    dev dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,17 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        lint: {
-            files: ['grunt.js', 'jquery.ui.touch-punch.js']
-        },
+
         jshint: {
             options: {
                 immed: false,
                 latedef: false,
                 browser: true,
                 eqeqeq: false
-            }
+            },
+            files: ['jquery.ui.touch-punch.js']
         },
 
-        min: {
+        uglify: {
             dist : {
                 src : ["jquery.ui.touch-punch.js"],
                 dest : "jquery.ui.touch-punch.min.js"
@@ -20,5 +19,8 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('default', 'lint min');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['jshint', 'uglify']);
 };
